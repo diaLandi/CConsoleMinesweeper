c95bfdc6a2e91e5cac57a664a658166d42a32503
David Landner <diaLandner@gmail.com>
2026-05-12 10:29:00 +0200

    Start

--- main.c
+++ main.c
@@ -3,8 +3,78 @@
 #include <time.h>
 #include <Windows.h>
 
-//https://prod.liveshare.vsengsaas.visualstudio.com/join?93F31F49BBC3E53B62319A678D0F7F0F41DE
+#define GAME_FIELD_HEIGHT 16
+#define GAME_FIELD_LEN 16
+#define MINE_COUNT 40
+
+int countBombs(char gameField[GAME_FIELD_LEN][GAME_FIELD_HEIGHT], int indexX, int indexY, int sizeX, int sizeY){
+    int counter = 0;
+    
+    if(indexX > sizeX || indexX < 0 || indexY > sizeY || indexY < 0){
+        return -1;
+    }
+    
+    for(int i = -1; i < 2; i++){
+        for(int j = -1; i < 2; j++){
+            if(indexX + i >= 0 && indexX + i < sizeX){
+                if(indexY + j >= 0 && indexY + j < sizeY){
+                    if(gameField[indexX + i][indexY + j]){
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+    
+    return counter;
+}
+
+void printFieldUser(char gameField[GAME_FIELD_LEN][GAME_FIELD_HEIGHT]){
+    printf("+-------------------------------+\n");
+
+    for(int i = 0; i < GAME_FIELD_HEIGHT; i++){
+        for(int j = 0; j < GAME_FIELD_LEN; j++){
+            printf("| %c ", gameField[j][i]);
+            if(j = GAME_FIELD_LEN - 1){
+                printf("|\n");
+            }
+        }
+
+        if(i < GAME_FIELD_HEIGHT - 1){
+            printf("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
+        }
+    }
+
+    printf("+-------------------------------+\n");
+}
+
+int menu(){
+    int fieldSize;
+
+    system("cls");
+    printf("How hard should the Game be?\n");
+    printf("Intermediate (1)\n\n");
+    printf("Your choice");
+    
+    do{
+        scanf("%d", &fieldSize);
+    } while(fieldSize != 1);
+    
+    switch (fieldSize) {
+    case 1:
+        fieldSize = 40;
+        break;
+    }
+
+    return fieldSize;
+}
+
+void firstMove(char gameField[GAME_FIELD_LEN][GAME_FIELD_HEIGHT]){
+    
+}
 
 int main(){
-    printf("");
+    char gameField[GAME_FIELD_LEN][GAME_FIELD_HEIGHT] = {'0'};
+
+    printFieldUser(gameField);
 }
--- standard.md
+++ standard.md
@@ -1,5 +1,8 @@
 □ - für unentdecke felder
 1-8 für Zahlen
 0 für leeres Feld
+9 für Bombe
 
 1 2D Feld
+
+//https://prod.liveshare.vsengsaas.visualstudio.com/join?93F31F49BBC3E53B62319A678D0F7F0F41DE
