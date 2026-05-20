67b9671867c62c713f2df8cdad64c811f4d91752
David Landner <diaLandner@gmail.com>
2026-05-20 07:51:39 +0200

    Changed definition for bombs

--- printing.c
+++ printing.c
@@ -8,7 +8,7 @@ void printFieldUser(char gameField[GAME_FIELD_LEN][GAME_FIELD_HEIGHT], int curso
     for(int i = 0; i < GAME_FIELD_HEIGHT; i++){
         for(int j = 0; j < GAME_FIELD_LEN; j++){
             if(cursorPositionX == j && cursorPositionY == i){
-                printf("| \033[1;35m%c\033[0m ", gameField[j][i]);
+                printf("| \033[6;35m%c\033[0m ", gameField[j][i]);
             } else{
                 switch (gameField[j][i]){
                 case '0':
